c246bc0720e8a2cafed93189e91f6f415e5bd666
AlexanderM33 <kd.murom@gmail.com>
2023-10-18 21:37:18 +0300

    task2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -39,3 +39,58 @@ variable "vms_ssh_root_key" {
   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0LzrwzAXpYNbzSVxrmEthMFDvNwFMVXNF/dSz1P3dN eddsa-key-20230928"
   description = "ssh-keygen -t ed25519"
 }
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "web name"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "platform type"
+}
+
+variable "vm_web_instance" {
+  type        = string
+  default     = "ubuntu-2204-lts"
+  description = "image name"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  default     = "2"
+  description = "cores"
+}
+
+variable "vm_web_memory" {
+  type        = number
+  default     = "1"
+  description = "memory"
+}
+variable "vm_web_core_fraction" {
+  type        = number
+  default     = "5"
+  description = "core_fraction"
+}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  default     = "true"
+  description = "preemptible"
+}
+
+variable "vm_web_nat" {
+  type        = bool
+  default     = "true"
+  description = "NAT"
+}
+
+variable "vm_web_serial_port" {
+  type        = number
+  default     = "1"
+  description = "serial_port"
+}
+
+
